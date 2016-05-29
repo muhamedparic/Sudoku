6f9c344d01404106bf07f856b7d7e7f2d5694a38
muhamedparic <mparic1@etf.unsa.ba>
2016-05-29 21:38:52 +0200

    Kod za server

--- Sudoku-Server/server.php
+++ Sudoku-Server/server.php
@@ -1,8 +1,16 @@
 <?php
 $difficulty = $_REQUEST['difficulty'];
 
-$grid = file_get_contents($difficulty . '.txt');
-unlink($difficulty . '.txt');
-echo $grid;
-shell_exec('./server ' . $difficulty);
+if (file_exists($difficulty . '.txt')) {
+	$grid = file_get_contents($difficulty . '.txt');
+	unlink($difficulty . '.txt');
+	echo $grid;
+	shell_exec('server ' . $difficulty);
+} else {
+	shell_exec('server ' . $difficulty);
+	$grid = file_get_contents($difficulty . '.txt');
+	unlink($difficulty . '.txt');
+	echo $grid;
+	shell_exec('server ' . $difficulty);
+}
 ?>
